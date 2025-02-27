3af73a540771acb37ee079205b0abf11ae89d049
AlbertWebs <albertmuhatia@gmail.com>
2025-02-27 19:15:03 +0300

    Update Kenya

---
+++ resources/views/admin/editProduct.blade.php
@@ -0,0 +1,196 @@
+@extends('admin.master')
+@section('content')
+<!-- Remember to include jQuery :) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+<!-- jQuery Modal -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+<style>
+    .modal a.close-modal{
+        top:0px !important;
+        right:0px !important;
+    }
+</style>
+<!--== BODY CONTNAINER ==-->
+ <div class="container-fluid sb2">
+    <div class="row">
+        @include('admin.sidebar')
+
+        <!--== BODY INNER CONTAINER ==-->
+
+        <div class="sb2-2">
+            <div class="sb2-2-2">
+                <ul>
+                    <li><a href="{{url('/')}}"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+                    </li>
+                    <li class="active-bre"><a href="#"> Edit {{$Product->name}}</a>
+                    </li>
+                    <li class="page-back"><a href="{{url('/')}}/admin/products"><i class="fa fa-backward" aria-hidden="true"></i> All Products</a>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="sb2-2-add-blog sb2-2-1">
+                <div class="box-inn-sp">
+                    <div class="inn-title">
+                        <h4>Edit {{$Product->name}}</h4>
+                        <center>
+                            @if(Session::has('message'))
+                                          <div class="alert alert-success">{{ Session::get('message') }}</div>
+                           @endif
+
+                           @if(Session::has('messageError'))
+                                          <div class="alert alert-danger">{{ Session::get('messageError') }}</div>
+                           @endif
+                        </center>
+                    </div>
+                    <div class="bor">
+                        <form method="POST" action="{{url('/')}}/admin/edit_Product/{{$Product->id}}" enctype="multipart/form-data">
+                            {{csrf_field()}}
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input autocomplete="off" name="title" id="list-title" value="{{$Product->title}}" type="text" class="validate" required>
+                                    <label for="list-title">Product Name</label>
+                                </div>
+                                <div class="input-field col s12">
+                                    <input autocomplete="off" name="price" id="list-title" value="{{$Product->price}}" type="number" class="validate" required>
+                                    <label for="list-title">Product Price</label>
+                                </div>
+
+
+
+                            </div>
+                            <div class="row">
+
+                                {{--  --}}
+                                <div class="input-field col s12">
+                                    <select required name="category" class="icons" id="mydiv">
+                                        <?php $CategorySelected = DB::table('categories')->where('id',$Product->category)->get() ?>
+                                        @foreach ($CategorySelected as $CatSel)
+                                        <option value="{{$CatSel->id}}" selected>{{$CatSel->title}}</option>
+                                        @endforeach
+                                        @foreach ($Category as $Categories)
+                                        <option value="{{$Categories->id}}" data-icon="{{url('/')}}/uploads/categories/{{$Categories->image}}" class="circle">{{$Categories->title}}</option>
+                                        @endforeach
+                                    </select>
+                                    <label>Choose Category</label>
+                                </div>
+
+                                <div class="section-space col s12"></div>
+                            </div>
+
+                            <div class="row">
+
+                                {{--  --}}
+                                <div class="input-field col s12">
+                                    <select required name="gown_class" class="icons" id="mydiv">
+                                        <?php
+                                            $GwonClass = DB::table('gown_classes')->where('id',$Product->slung)->get();
+                                            $GClass = DB::table('gown_classes')->get();
+                                         ?>
+                                        @foreach ($GwonClass as $gownclass)
+                                        <option value="{{$gownclass->slung}}" selected>{{$gownclass->title}}</option>
+                                        @endforeach
+
+                                        @foreach ($GClass as $gclass)
+                                        <option value="{{$gclass->slung}}" data-icon="{{$gclass->image}}" class="circle">{{$gclass->title}}</option>
+                                        @endforeach
+                                    </select>
+                                    <label>Choose Gown Class</label>
+                                </div>
+
+                                <div class="section-space col s12"></div>
+                            </div>
+
+
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <textarea required id="article-ckeditor" name="content" class="materialilze-textarea" placeholder="content">{{$Product->content}}</textarea>
+                                </div>
+                            </div><br><br>
+
+
+
+
+                            <script src="https://amanivehiclesounds.co.ke/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+                            <script>
+                                CKEDITOR.replace( 'article_ckeditor' );
+                            </script>
+
+
+                           {{-- Images --}}
+                            <style>
+                                .btn-file {
+                                    position: relative;
+                                    overflow: hidden;
+                                }
+                                .btn-file input[type=file] {
+                                    position: absolute;
+                                    top: 0;
+                                    right: 0;
+                                    min-width: 33.33%;
+                                    min-height: 100%;
+                                    font-size: 100px;
+                                    text-align: right;
+                                    filter: alpha(opacity=0);
+                                    opacity: 0;
+                                    outline: none;
+                                    background: white;
+                                    cursor: inherit;
+                                    display: block;
+                                }
+
+                                #img-upload{
+                                    width: 33.33%;
+                                }
+                                .image-preview{
+                                    max-height:100%;
+                                    height:100% !important;
+                                }
+                            </style>
+                            {{-- Style --}}
+                            <div class="row">
+                                <div class="">
+                                    <div class="input-field col s3 col-lg-6">
+                                        <div class="form-group">
+                                            <label>Image One</label>
+                                            <div class="input-group">
+                                                <span class="input-group-btn">
+                                                    <span class="btn btn-default btn-file">
+                                                       <small> One: Size 277 by 377  Browse… </small><input name="image_one" type="file" id="imgInp">
+                                                    </span>
+                                                </span>
+                                                <input type="text" class="form-control" readonly>
+                                            </div>
+                                            <img class="image-preview" style="width:100%" src="{{$Product->image}}"/>
+                                        </div>
+                                    </div>
+
+
+
+
+                                </div>
+                            </div>
+
+                            <input type="hidden" name="image_one_cheat" value="{{$Product->image}}">
+                            <input type="hidden" name="image_two_cheat" value="{{$Product->image_two}}">
+                            <input type="hidden" name="image_three_cheat" value="{{$Product->image_three}}">
+                            <input type="hidden" name="image_four_cheat" value="{{$Product->image_four}}">
+
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input  type="submit" class="waves-effect waves-light btn-large" value="Save Changes">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--== BODY INNER CONTAINER ==-->
+
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminsController;
 
 
 
@@ -31,4 +32,230 @@ Route::get('/blog', [App\Http\Controllers\HomeController::class, 'blog'])->name(
 Route::get('/blog/{slung}', [App\Http\Controllers\HomeController::class, 'blog_single'])->name('blog-single');
 
 
+
+
+Auth::routes();
+
+// group routes with prefix
+Route::prefix('admin')->group(function () {
+
+    Route::get('/', [AdminsController::class, 'adminHome'])->name('admin.home');
+    Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+    // SiteSettings
+    Route::get('/credentials', [AdminsController::class, 'systemsCredentials']);
+    Route::get('/SiteSettings', [AdminsController::class, 'SiteSettings']);
+    Route::get('/SocialMediaSettings', [AdminsController::class, 'SocialMediaSettings']);
+    Route::get('/logo-and-favicon', [AdminsController::class, 'logo_and_favicon']);
+    Route::post('/logo-and-favicon-update', [AdminsController::class, 'logo_and_favicon_update']);
+
+    // mailerSettings
+    Route::get('/mailerSettings', [AdminsController::class, 'mailerSettings']);
+
+    // Terms and Conditions
+    Route::get('/terms', [AdminsController::class, 'terms']);
+    Route::get('/addTerms', [AdminsController::class, 'addTerms']);
+    Route::get('/editTerm/{id}', [AdminsController::class, 'editTerm']);
+    Route::post('/add_term', [AdminsController::class, 'add_term']);
+    Route::post('/edit_term/{id}', [AdminsController::class, 'edit_term']);
+    Route::get('/delete_term/{id}', [AdminsController::class, 'delete_term']);
+
+    // Privacy Policy
+    Route::get('/privacy', [AdminsController::class, 'privacy']);
+    Route::get('/addPrivacy', [AdminsController::class, 'addPrivacy']);
+    Route::get('/editPrivacy/{id}', [AdminsController::class, 'editPrivacy']);
+    Route::post('/add_privacy', [AdminsController::class, 'add_privacy']);
+    Route::post('/edit_privacy/{id}', [AdminsController::class, 'edit_privacy']);
+    Route::get('/delete_privacy/{id}', [AdminsController::class, 'delete_privacy']);
+
+    // Frequently Asked Questions
+    Route::get('/faq', [AdminsController::class, 'faq']);
+    Route::get('/addFaq', [AdminsController::class, 'addFaq']);
+    Route::get('/editFaq/{id}', [AdminsController::class, 'editFaq']);
+    Route::post('/add_Faq', [AdminsController::class, 'add_Faq']);
+    Route::post('/edit_Faq/{id}', [AdminsController::class, 'edit_Faq']);
+    Route::get('/delete_Faq/{id}', [AdminsController::class, 'delete_Faq']);
+
+    // Risks Declaration
+    Route::get('/editRisk', [AdminsController::class, 'editRisk']);
+    Route::post('/edit_Risk', [AdminsController::class, 'edit_Risk']);
+
+    // How It Works
+    Route::get('/how', [AdminsController::class, 'how']);
+    Route::get('/addHow', [AdminsController::class, 'addHow']);
+    Route::get('/editHow/{id}', [AdminsController::class, 'editHow']);
+    Route::post('/add_How', [AdminsController::class, 'add_How']);
+    Route::post('/edit_How/{id}', [AdminsController::class, 'edit_How']);
+    Route::get('/delete_How/{id}', [AdminsController::class, 'delete_How']);
+
+    // Courses
+    Route::get('/courses', [AdminsController::class, 'courses']);
+    Route::get('/addCourse', [AdminsController::class, 'addCourse']);
+    Route::get('/editCourse/{id}', [AdminsController::class, 'editCourse']);
+    Route::post('/add_Course', [AdminsController::class, 'add_Course']);
+    Route::post('/edit_Course/{id}', [AdminsController::class, 'edit_Course']);
+    Route::get('/delete_Course/{id}', [AdminsController::class, 'delete_Course']);
+
+    // Topics
+    Route::get('/topics', [AdminsController::class, 'topics']);
+    Route::get('/addTopic', [AdminsController::class, 'addTopic']);
+    Route::get('/editTopic/{id}', [AdminsController::class, 'editTopic']);
+    Route::post('/add_Topic', [AdminsController::class, 'add_Topic']);
+    Route::post('/edit_Topic/{id}', [AdminsController::class, 'edit_Topic']);
+    Route::get('/delete_Topic/{id}', [AdminsController::class, 'delete_Topic']);
+
+    // Copyright Statement
+    Route::get('/editCopyright', [AdminsController::class, 'copyright']);
+    Route::post('/edit_Copyright', [AdminsController::class, 'edit_copyright']);
+
+    // About Us
+    Route::get('/editAbout', [AdminsController::class, 'about']);
+    Route::post('/edit_About', [AdminsController::class, 'edit_about']);
+
+    // Sliders
+    Route::get('/sliders', [AdminsController::class, 'slider']);
+    Route::get('/addSlider', [AdminsController::class, 'addSlider']);
+    Route::post('/add_Slider', [AdminsController::class, 'add_Slider']);
+    Route::get('/editSlider/{id}', [AdminsController::class, 'editSlider']);
+    Route::post('/edit_Slider/{id}', [AdminsController::class, 'edit_Slider']);
+    Route::get('/deleteSlider/{id}', [AdminsController::class, 'deleteSlider']);
+
+    // Banners
+    Route::get('/banners', [AdminsController::class, 'banners']);
+    Route::get('/editBanner/{id}', [AdminsController::class, 'editBanner']);
+    Route::post('/edit_Banner/{id}', [AdminsController::class, 'edit_Banner']);
+
+    // Messages
+    Route::get('/allMessages', [AdminsController::class, 'allMessages']);
+    Route::get('/unread', [AdminsController::class, 'unread']);
+    Route::post('/reply', [AdminsController::class, 'reply']);
+    Route::get('/read/{id}', [AdminsController::class, 'read']);
+    Route::get('/deleteMessage/{id}', [AdminsController::class, 'deleteMessage']);
+
+    // Categories
+    Route::get('/categories', [AdminsController::class, 'categories']);
+    Route::get('/addCategory', [AdminsController::class, 'addCategory']);
+    Route::post('/add_Category', [AdminsController::class, 'add_Category']);
+    Route::get('/editCategories/{id}', [AdminsController::class, 'editCategories']);
+    Route::post('/edit_Category/{id}', [AdminsController::class, 'edit_Category']);
+    Route::get('/deleteCategory/{id}', [AdminsController::class, 'deleteCategory']);
+
+    // Products
+    Route::get('/products', [AdminsController::class, 'products']);
+    Route::get('/addProduct', [AdminsController::class, 'addProduct']);
+    Route::post('/add_Product', [AdminsController::class, 'add_Product']);
+    Route::get('/editProducts/{id}', [AdminsController::class, 'editProducts']);
+    Route::post('/edit_Product/{id}', [AdminsController::class, 'edit_Product']);
+    Route::get('/deleteProduct/{id}', [AdminsController::class, 'deleteProduct']);
+
+
+
+    Route::post('/add_Variation', [AdminsController::class, 'add_Variation']);
+    Route::post('/edit_Variation/{id}', [AdminsController::class, 'edit_Variation']);
+    Route::get('/addVariation/color', [AdminsController::class, 'variations_color']);
+    Route::get('/addVariation/size', [AdminsController::class, 'variations_size']);
+    Route::get('/variations/color', [AdminsController::class, 'var_color']);
+    Route::get('/variations/size', [AdminsController::class, 'var_size']);
+    Route::get('/editVariation/{id}', [AdminsController::class, 'editVariation']);
+
+    // Categories
+    Route::get('/categories', [AdminsController::class, 'categories']);
+    Route::get('/addCategory', [AdminsController::class, 'addCategory']);
+    Route::post('/add_Category', [AdminsController::class, 'add_Category']);
+    Route::get('/editCategories/{id}', [AdminsController::class, 'editCategories']);
+    Route::post('/edit_Category/{id}', [AdminsController::class, 'edit_Category']);
+    Route::get('/deleteCategory/{id}', [AdminsController::class, 'deleteCategory']);
+
+    // Testimonials
+    Route::get('/testimonials', [AdminsController::class, 'testimonials']);
+    Route::get('/addTestimonial', [AdminsController::class, 'addTestimonial']);
+    Route::post('/add_Testimonial', [AdminsController::class, 'add_Testimonial']);
+    Route::get('/editTestimonial/{id}', [AdminsController::class, 'editTestimonial']);
+    Route::post('/edit_Testimonial/{id}', [AdminsController::class, 'edit_Testimonial']);
+
+    // Signal
+    Route::get('/signals', [AdminsController::class, 'signals']);
+    Route::get('/addSignal', [AdminsController::class, 'addSignal']);
+    Route::post('/add_Signal', [AdminsController::class, 'add_Signal']);
+    Route::get('/editSignal/{id}', [AdminsController::class, 'editSignal']);
+    Route::post('/edit_Signal/{id}', [AdminsController::class, 'edit_Signal']);
+
+    // Blog
+    Route::get('/blog', [AdminsController::class, 'blog']);
+    Route::get('/addBlog', [AdminsController::class, 'addBlog']);
+    Route::post('/add_Blog', [AdminsController::class, 'add_Blog']);
+    Route::get('/editBlog/{id}', [AdminsController::class, 'editBlog']);
+    Route::post('/edit_Blog/{id}', [AdminsController::class, 'edit_Blog']);
+    Route::get('/delete_Blog/{id}', [AdminsController::class, 'delete_Blog']);
+
+
+    // Payments
+    Route::get('/b2b', [AdminsController::class, 'b2b']);
+    Route::get('/b2c', [AdminsController::class, 'b2c']);
+    Route::get('/lnmo_api_response', [AdminsController::class, 'lnmo_api_response']);
+    Route::get('/mobile_payments', [AdminsController::class, 'mobile_payments']);
+    Route::get('/reverse_transaction', [AdminsController::class, 'reverse_transaction']);
+    Route::get('/transaction_status', [AdminsController::class, 'transaction_status']);
+    Route::get('/accountbalance', [AdminsController::class, 'accountbalance']);
+    Route::get('/approve-transaction/{id}', [AdminsController::class, 'approve_transaction']);
+    Route::get('/approve-transaction-stk/{id}', [AdminsController::class, 'approve_transaction_stk']);
+
+    // Enroll Users
+    Route::get('/enroll-users', [AdminsController::class, 'enroll_users']);
+    Route::get('/enroll-users-post/{id}', [AdminsController::class, 'enroll_users_post']);
+    Route::post('/enroll-user-now', [AdminsController::class, 'enroll_user_now']);
+
+    //Manage Users
+    Route::get('/users', [AdminsController::class, 'users']);
+    Route::get('/admins', [AdminsController::class, 'admins']);
+    Route::get('/addUser', [AdminsController::class, 'addUser']);
+    Route::get('/editUser/{id}', [AdminsController::class, 'editUser']);
+    Route::post('/add_User', [AdminsController::class, 'add_User']);
+    Route::post('/edit_User/{id}', [AdminsController::class, 'edit_User']);
+    Route::get('/delete_user/{id}', [AdminsController::class, 'delete_user']);
+    Route::get('/switchRole/{id}', [AdminsController::class, 'switchRole']);
+    Route::get('/switchStatus/{id}', [AdminsController::class, 'switchStatus']);
+
+    Route::get('/updateSlung', [AdminsController::class, 'updateSlung']);
+
+
+
+
+    Route::get('/activitylogs', [AdminsController::class, 'activitylogs']);
+
+
+    // AJAX REQUESTS
+    Route::post('/addCategoryAjaxRequest', [AdminsController::class, 'addCategoryAjaxRequest']);
+    Route::post('/deleteBlogAjax', [AdminsController::class, 'deleteBlogAjax']);
+    Route::put('/updateSiteSettingsAjax', [AdminsController::class, 'updateSiteSettingsAjax']);
+    Route::put('/updateMailerAjax', [AdminsController::class, 'updateMailerAjax']);
+    Route::put('/updateSiteSocialMediaAjax', [AdminsController::class, 'updateSiteSocialMediaAjax']);
+    Route::put('/updateSiteCredentials', [AdminsController::class, 'updateSiteCredentials']);
+    Route::post('/deleteCategoryAjax', [AdminsController::class, 'deleteCategoryAjax']);
+    Route::post('/deleteTestimonialAjax', [AdminsController::class, 'deleteTestimonialAjax']);
+    Route::post('/deleteSliderAjax', [AdminsController::class, 'deleteSliderAjax']);
+    Route::post('/deleteC2BAjax', [AdminsController::class, 'deleteC2BAjax']);
+    Route::post('/deleteB2BAjax', [AdminsController::class, 'deleteB2BAjax']);
+    Route::post('/deleteB2CAjax', [AdminsController::class, 'deleteB2CAjax']);
+    Route::post('/deleteBalAjax', [AdminsController::class, 'deleteBalAjax']);
+    Route::post('/deleteSTKAjax', [AdminsController::class, 'deleteSTKAjax']);
+    Route::post('/deleteReverseAjax', [AdminsController::class, 'deleteReverseAjax']);
+    Route::post('/deleteTransactionAjax', [AdminsController::class, 'deleteTransactionAjax']);
+    Route::post('/deleteUserAjax', [AdminsController::class, 'deleteUserAjax']);
+    Route::post('/deleteFaqAjax', [AdminsController::class, 'deleteFaqAjax']);
+    Route::post('/deletePrivacyAjax', [AdminsController::class, 'deletePrivacyAjax']);
+    Route::post('/deleteTermsAjax', [AdminsController::class, 'deleteTermsAjax']);
+    Route::post('/deleteHowAjax', [AdminsController::class, 'deleteHowAjax']);
+    Route::post('/deleteCoursesAjax', [AdminsController::class, 'deleteCoursesAjax']);
+    Route::post('/deleteTopicsAjax', [AdminsController::class, 'deleteTopicsAjax']);
+    Route::post('/deleteSignalsAjax', [AdminsController::class, 'deleteSignalsAjax']);
+    Route::post('/deleteProductAjax', [AdminsController::class, 'deleteProductAjax']);
+
+    // SocialMedia
+    Route::get('/facebook', [LoginController::class, 'facebook']);
+    Route::get('/facebook/redirect', [LoginController::class, 'facebookRedirect']);
+    Route::get('/google', [LoginController::class, 'google']);
+    Route::get('/google/redirect', [LoginController::class, 'googleRedirect']);
+
+});
 
