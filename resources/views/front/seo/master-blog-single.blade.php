16b738d59689ff0d9887425b2717f56e681d00d4
AlbertWebs <albertmuhatia@gmail.com>
2025-04-05 14:58:52 +0300

    update

--- resources/views/front/seo/master-blog-single.blade.php
+++ resources/views/front/seo/master-blog-single.blade.php
@@ -9,7 +9,7 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
         {{--  --}}
         <title>{{$Blog->title}} -Graduation Gowns for Hire - Gownsea LTD </title>
         <meta name="description" content="{{$Blog->meta}}">
-        <meta name="keywords" content="{{$Blog->tag}}">
+        <meta name="keywords" content="{{$Blog->tags}}">
         <meta name="author" content="Gownsea LTD">
         <meta name="robots" content="index, follow">
         <link rel="canonical" href="{{url('/')}}/blog/{{$Blog->slung}}" />
