9e7292c3163ef408403711244b54a6ec1eaff72b
AlbertWebs <albertmuhatia@gmail.com>
2025-08-25 16:09:21 +0300

    update ladng optmized

--- resources/views/front/seo/master.blade.php
+++ resources/views/front/seo/master.blade.php
@@ -41,6 +41,12 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
 
 
 
+        <!-- ✅ Preload critical assets -->
+        <link rel="preload" as="image" href="{{url('/')}}/uploads/categories/Gownsea-graduation-gowns.webp">
+        <link rel="preload" as="style" href="{{asset('theme/css/style.css')}}">
+        <link rel="preload" as="font" href="{{asset('theme/fonts/custom.woff2')}}" type="font/woff2" crossorigin>
+
+
         <!-- Schema Markup (Google Rich Results) -->
         <script type="application/ld+json">
         {
@@ -100,22 +106,24 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
         <!-- Favicon -->
         @include('favicon')
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="{{asset('theme/css/bootstrap.min.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/style.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/style-responsive.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/vertical-rhythm.min.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/magnific-popup.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/owl.carousel.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/splitting.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/YTPlayer.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/demo-corporate/demo-corporate.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/demo-brutalist/demo-brutalist.css')}}">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <!--Floating WhatsApp css-->
-        <link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
-        <link rel="stylesheet" href="{{asset('theme/css/style-two.css')}}">
+        <!-- ✅ CSS moved to load async -->
+        <link rel="stylesheet" href="{{asset('theme/css/bootstrap.min.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/style.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/style-responsive.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/vertical-rhythm.min.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/magnific-popup.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/owl.carousel.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/splitting.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/YTPlayer.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/demo-corporate/demo-corporate.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/demo-brutalist/demo-brutalist.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="{{asset('theme/css/style-two.css')}}" media="print" onload="this.media='all'">
+        <link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css" media="print" onload="this.media='all'">
 
+        <noscript>
+            <!-- Fallback if JS is off -->
+            <link rel="stylesheet" href="{{asset('theme/css/style.css')}}">
+        </noscript>
 
     </head>
     <body class="appear-animate">
