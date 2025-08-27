77166e5fd1275a27ec46095f577a73f73e459bee
AlbertWebs <albertmuhatia@gmail.com>
2025-08-27 10:19:23 +0300

    disable YT

--- resources/views/front/seo/master.blade.php
+++ resources/views/front/seo/master.blade.php
@@ -108,7 +108,7 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
         <link rel="stylesheet" href="{{asset('theme/css/magnific-popup.css')}}">
         <link rel="stylesheet" href="{{asset('theme/css/owl.carousel.css')}}">
         <link rel="stylesheet" href="{{asset('theme/css/splitting.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/YTPlayer.css')}}">
+        {{-- <link rel="stylesheet" href="{{asset('theme/css/YTPlayer.css')}}"> --}}
         <link rel="stylesheet" href="{{asset('theme/css/demo-corporate/demo-corporate.css')}}">
         <link rel="stylesheet" href="{{asset('theme/css/demo-brutalist/demo-brutalist.css')}}">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
