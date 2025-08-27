b665db3a6ecd9217ff53905e7fbbce7c9c94c0d0
AlbertWebs <albertmuhatia@gmail.com>
2025-08-27 11:14:15 +0300

    upate

--- resources/views/front/seo/master.blade.php
+++ resources/views/front/seo/master.blade.php
@@ -104,16 +104,16 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
         <link rel="stylesheet" href="{{asset('theme/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('theme/css/style.css')}}">
         <link rel="stylesheet" href="{{asset('theme/css/style-responsive.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/vertical-rhythm.min.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/magnific-popup.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/owl.carousel.css')}}">
-        <link rel="stylesheet" href="{{asset('theme/css/splitting.css')}}">
+        <link rel="stylesheet" href="{{asset('theme/css/vertical-rhythm.min.css')}}" defer>
+        <link rel="stylesheet" href="{{asset('theme/css/magnific-popup.css')}}" defer>
+        <link rel="stylesheet" href="{{asset('theme/css/owl.carousel.css')}}" defer>
+        <link rel="stylesheet" href="{{asset('theme/css/splitting.css')}}" defer>
         {{-- <link rel="stylesheet" href="{{asset('theme/css/YTPlayer.css')}}"> --}}
         <link rel="stylesheet" href="{{asset('theme/css/demo-corporate/demo-corporate.css')}}">
         <link rel="stylesheet" href="{{asset('theme/css/demo-brutalist/demo-brutalist.css')}}">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <!--Floating WhatsApp css-->
-        <link  rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
+        <link  rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css" defer>
         <link rel="stylesheet" href="{{asset('theme/css/style-two.css')}}">
 
 
@@ -123,7 +123,7 @@ You can find the code of your language here - https://www.w3schools.com/tags/ref
         <div style="z-index:100000;" id="WAButton"></div>
 
         <!-- Page Loader -->
-        <div class="page-loader color">
+        <div class="page-loader color"> 
             <div class="loader">Loading...</div>
         </div>
         <!-- End Page Loader -->
